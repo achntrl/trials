1762b40d62a40e97e48f20c4835b77539317a2f1
Alexandre Chaintreuil <alexandre.chaintreuil@gmail.com>
2017-11-25 12:27:15 +0100

    Handle 2 blocks collisions

--- src/CliniqueTrials.jsx
+++ src/CliniqueTrials.jsx
@@ -19,6 +19,52 @@ class CliniqueTrials extends Component {
     };
   }
 
+  componentWillMount() {
+    let trials = this.state.trials;
+    const startTrials = _.sortBy(trials, ['start']);
+    const endTrials = _.sortBy(trials, ['end']);
+
+    const startEvents = _.map(startTrials, function (trial){ return {'date': trial.start, 'event': 'start'} });
+    const endEvents = _.map(endTrials, function (trial){ return {'date': trial.end, 'event': 'end'} });
+
+    const allEvents = _.sortBy( _.concat(startEvents,endEvents), ['date'])
+
+
+    let collisions = {0:0}
+    let acc = 0
+    for (let i=0; i < allEvents.length; ++i) {
+      let anEvent = allEvents[i]
+      acc += anEvent.event == 'start' ? +1 : -1
+      collisions[anEvent.date] = acc
+    }
+
+    let locationOfCollisions = _.compact(_.map(collisions, (val, key) => {if (val > 1) return key}))
+
+    let findTrialsAtCollision = (position, trials) => {
+      return _.filter(trials, trial => {
+        return trial.start <= position && position <= trial.end;
+      })
+    }
+
+    let handleCollision = (collidings) => {
+      if (collidings[0].position === undefined && collidings[1].position === undefined) {
+        collidings[0].position = 0;
+        collidings[1].position = 1;
+      } else if (collidings[0].position === undefined) {
+        collidings[0].position = 1 - collidings[1].position;
+      } else if (collidings[1].position === undefined) {
+        collidings[1].position = 1 - collidings[0].position;
+      }
+      collidings[0].collide = true
+      collidings[1].collide = true
+    }
+
+    _.map(locationOfCollisions, (collision) => {
+      handleCollision(findTrialsAtCollision(parseInt(collision, 10), trials));
+    } )
+    console.log(trials)
+    this.setState({trials});
+  }
   widthMonthToPx = (month) => { return this.state.step * month}
 
   renderTrial = (trial) => {
@@ -26,8 +72,8 @@ class CliniqueTrials extends Component {
       start={this.widthMonthToPx(trial.start)}
       end={this.widthMonthToPx(trial.end)}
       height={this.state.height}
-      collisionFactor={trial.start % 2 ? 1 : 2}
-      position={trial.end % 2 ? 0 : 1}
+      collisionFactor={trial.collide ? 2 : 1}
+      position={trial.position ? trial.position : 0}
       title={trial.title}
       key={trial.title}
     />;
